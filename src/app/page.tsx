a14afa6a2afd7f0b7f93db6a861f12442d5272f0
Varun Shembekar <varun247shembekar@gmail.com>
2024-09-28 15:09:22 -0700

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,109 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import {
+  CopilotTask,
+  useCopilotContext,
+  useMakeCopilotReadable,
+} from "@copilotkit/react-core";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import Header from "@/components/header";
+import Sidebar from "@/components/sidebar";
+import PreviewScreen from "@/components/preview-screen";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [code, setCode] = useState<string[]>([
+    `<h1 class="text-red-500">Hello World</h1>`,
+  ]);
+  const [codeToDisplay, setCodeToDisplay] = useState<string>(code[0] || "");
+  const [showDialog, setShowDialog] = useState<boolean>(false);
+  const [codeCommand, setCodeCommand] = useState<string>("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const context = useCopilotContext();
+
+  const readableCode = useMakeCopilotReadable(codeToDisplay);
+
+  const generateCode = new CopilotTask({
+    instructions: codeCommand,
+    actions: [
+      {
+        name: "generateCode",
+        description: "Create Code Snippet with React.js, tailwindcss.",
+        parameters: [
+          {
+            name: "code",
+            type: "string",
+            description: "Code to be generated",
+            required: true,
+          },
+        ],
+        handler: async ({ code }) => {
+          setCode((prev) => [...prev, code]);
+          setCodeToDisplay(code);
+        },
+      },
+    ],
+  });
+
+  return (
+    <>
+      <main className="bg-white min-h-screen px-4">
+        <Header openCode={() => setShowDialog(true)} />
+        <div className="w-full h-full min-h-[70vh] flex justify-between gap-x-1 ">
+          <Sidebar>
+            <div className="space-y-2">
+              {code.map((c, i) => (
+                <div
+                  key={i}
+                  className="w-full h-20 p-1 rounded-md bg-white border border-blue-600"
+                  onClick={() => setCodeToDisplay(c)}
+                >
+                  v{i}
+                </div>
+              ))}
+            </div>
+          </Sidebar>
+
+          <div className="w-10/12">
+            <PreviewScreen html_code={codeToDisplay || ""} />
+          </div>
+        </div>
+        <div className="w-8/12 mx-auto p-1 rounded-full bg-primary flex my-4 outline-0">
+          <Input
+            type="text"
+            placeholder="Enter your code command"
+            className="w-10/12 p-6 rounded-l-full  outline-0 bg-primary text-white"
+            value={codeCommand}
+            onChange={(e) => setCodeCommand(e.target.value)}
+          />
+          <button
+            className="w-2/12 bg-white text-primary rounded-r-full"
+            onClick={() => generateCode.run(context)}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Generate
+          </button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <Dialog open={showDialog} onOpenChange={setShowDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>View Code.</DialogTitle>
+            <DialogDescription>
+              You can use the following code to start integrating into your
+              application.
+            </DialogDescription>
+            <div className="p-4 rounded bg-primary text-white my-2">
+              {readableCode}
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
